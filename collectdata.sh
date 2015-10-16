c27e2941348167b439e4ecac8b0ecaaf282b274e
Daniel Guzmán Burgos <nethalo@gmail.com>
2015-10-16 12:14:17 -0500

    Create collectdata.sh
    File to gather and send data to graphite

---
+++ collectdata.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+query="set session group_concat_max_len=2048; select concat(digest_text,'dash', round(sum_timer_wait/ 1000000000000, 6),'\n') from performance_schema.events_statements_summary_by_digest order by sum_timer_wait desc limit 20;"
+IFS="
+"
+for i in $(mysql -h54.163.131.13 -Nr -e"$query"); do
+        digest=$(echo ${i%%dash*})
+        digest=${digest%%,*}
+        digest=$(echo $digest | tr -d "\`")
+        digest=$(echo $digest | tr " " "_")
+        digest=$(echo $digest | tr -d "?")
+        digest=$(echo $digest | tr "." "-")
+        digest=$(echo $digest | tr "(" "_")
+        digest=$(echo $digest | tr ")" "_")
+        value=$(echo ${i##*dash})
+        echo "mysql.rds.$digest $value $(date +%s)" | nc -w 1 localhost 2003
+done
