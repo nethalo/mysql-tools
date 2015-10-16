0b7c69d13d016e54dd00950adce1a2ce5477a476
Daniel Guzmán Burgos <nethalo@gmail.com>
2015-10-16 12:15:26 -0500

    remove the ip from mysql host example

--- collectdata.sh
+++ collectdata.sh
@@ -2,7 +2,7 @@
 query="set session group_concat_max_len=2048; select concat(digest_text,'dash', round(sum_timer_wait/ 1000000000000, 6),'\n') from performance_schema.events_statements_summary_by_digest order by sum_timer_wait desc limit 20;"
 IFS="
 "
-for i in $(mysql -h54.163.131.13 -Nr -e"$query"); do
+for i in $(mysql -Nr -e"$query"); do
         digest=$(echo ${i%%dash*})
         digest=${digest%%,*}
         digest=$(echo $digest | tr -d "\`")
